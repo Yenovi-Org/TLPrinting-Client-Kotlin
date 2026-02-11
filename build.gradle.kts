ae7331338ea1db3c2608b85c253a4561e2d5e615
kemkriszt <kemkriszt@gmail.com>
2026-02-12 00:06:45 +0200

    Add release github action

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     kotlin("jvm") version "2.3.0"
+    `maven-publish`
 }
 
 group = "com.yenovi"
-version = "0.1"
+version = System.getenv("VERSION") ?: "0.1"
 
 repositories {
     mavenCentral()
@@ -19,4 +20,22 @@ kotlin {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Yenovi-Org/TLPrinting-Client-Kotlin")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
